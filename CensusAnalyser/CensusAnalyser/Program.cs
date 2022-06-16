b6e80f8ab59508e785c87454811f8fb5d462b09a
Reshma2310 <dreshma253@gmail.com>
2022-06-16 14:47:39 +0500

    [Reshma] - Read Indian States Census Informaton from CSV File

---
+++ CensusAnalyser/CensusAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CensusAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\StateCensusData.csv";
+            StateAnalyser getMethod = new StateAnalyser();
+            getMethod.DataAnalyser(filePath);
+        }
+    }
+}
---
+++ CensusAnalyser/CensusAnalyser/StateAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace CensusAnalyser
+{
+    public class StateAnalyser
+    {
+        public int DataAnalyser(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var details = csv.GetRecords<StateModelClass>().ToList();
+                var numberOfRecords = details.Count();
+                foreach (var data in details)
+                {
+                    Console.WriteLine("State: " + data.State + "\nPopulation: " + data.Population + "\nAreaInSqKm: " + data.AreaInSqKm
+                        + "\nDensityPerSqKm: " + data.DensityPerSqKm + "\n");
+                }
+                return numberOfRecords;
+            }
+        }
+    }
+}
