9fe89befdb129e3ecdb3b252a4f098b028cf2159
Reshma2310 <dreshma253@gmail.com>
2022-06-16 22:44:13 +0530

    [RESHMA] - Performing 5 test cases - UC2

--- CensusAnalyser/CensusAnalyserTesting/UnitTest1.cs
+++ CensusAnalyser/CensusAnalyserTesting/UnitTest1.cs
@@ -1,8 +1,8 @@
 namespace CensusAnalyserTesting
 {
     public class Tests
-    {        
-        [Test]
+    {
+        /*[Test]
         public void GivenStatesCensusCSVFile_CheckNumberOfRecordsMatches()
         {
             int expected = 29;
@@ -68,6 +68,79 @@ namespace CensusAnalyserTesting
                 string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\InvalidHeaderCensusData.csv";
                 CensusAnalyser.StateAnalyser value = new CensusAnalyser.StateAnalyser();
                 int actual = value.DataAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Incorrect Header", ex.Message);
+            }
+        }*/
+        [Test]
+        public void GivenStatesCodeCSVFile_CheckNumberOfRecordsMatches()
+        {
+            int expected = 37;
+            string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\StateCode.csv";
+            CensusAnalyser.StateAnalyser exa = new CensusAnalyser.StateAnalyser();
+            int actual = exa.StateCodeAnalyser(filePath);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void GivenInvalidCodeFile_ShouldThrowInvalidFileException()
+        {
+            try
+            {
+                int expected = 37;
+                string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\StateCensusData.csv";
+                CensusAnalyser.StateAnalyser data = new CensusAnalyser.StateAnalyser();
+                int actual = data.StateCodeAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid File", ex.Message);
+            }
+        }
+        [Test]
+        public void GivenInvalidCodeFileType_ShouldThrowInvalidFileTypeException()
+        {
+            try
+            {
+                int expected = 37;
+                string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\StateCensusData.txt";
+                CensusAnalyser.StateAnalyser value = new CensusAnalyser.StateAnalyser();
+                int actual = value.StateCodeAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid File Type", ex.Message);
+            }
+        }
+        [Test]
+        public void GivenInvalidDelimiterFile_ShouldThrowIncorrectDelimiterException()
+        {
+            try
+            {
+                int expected = 37;
+                string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\InvaliDelimeterStateCode.csv";
+                CensusAnalyser.StateAnalyser data = new CensusAnalyser.StateAnalyser();
+                int actual = data.StateCodeAnalyser(filePath);
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Incorrect Delimiter", ex.Message);
+            }
+        }
+        [Test]
+        public void GivenInvalidHeaderFile_ShouldThrowIncorrectHeaderException()
+        {
+            try
+            {
+                int expected = 37;
+                string filePath = @"D:\BridgeLabs\CensusAnalyser\CensusAnalyser\CensusAnalyser\InvalidHeaderStateCode.csv";
+                CensusAnalyser.StateAnalyser value = new CensusAnalyser.StateAnalyser();
+                int actual = value.StateCodeAnalyser(filePath);
                 Assert.AreEqual(expected, actual);
             }
             catch (Exception ex)
